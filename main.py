dad7ebc5ac0bde53889334929e9a54906e6989d3
aasverg <aasvergd@mail.ru>
2024-02-13 11:25:38 +0300

    Улучшил инициализацию, добавил функцию projectInfo.

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 class Student:
-    def __init__(self, id, name, school, _class, score):
-        self.id = id
+    def __init__(self, id, name, titleProject_id, _class, score):
+        self.id = int(id)
         self.name = name
-        self.school = school
+        self.titleProject_id = int(titleProject_id)
         self._class = _class
-        self.score = score
+        if score == 'None':
+            self.score = 0
+        else:
+            self.score = int(score)
 
+def projectInfo(students, id):
+    for student in students:
+        if student.titleProject_id == id:
+            print(f"Проект №{id}:\nУченик: {student.name}.\nКласс: {student._class}.\nОценка за проект: {student.score}.")
+            break
 
-f = open('students.txt', 'r')
+f = open('students.txt', 'r', encoding='utf-8')
 f.readline()
 students = []
 for i in f:
-    students.append(Student(*i.split(',')))
+    students.append(Student(*i.strip().split(',')))
 
-for i in range(len(students)):
-    pass
+for i in range(1, len(students)):
+    j = i-1
+    sortingStudent = students[i]
+    while j >= 0 and sortingStudent.score > students[j].score:
+        students[j+1] = students[j]
+        j -= 1
+    students[j + 1] = sortingStudent
+
+for i in range(1, 4):
+    print(f"{i} место: {students[i-1].name}")
+
+print()
+projectInfo(students, 304)
